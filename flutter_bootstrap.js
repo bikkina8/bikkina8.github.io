77b4bfe3734012ec70eda5a3ef20f0d2bff94a19
github_username <email_address>
2025-03-30 22:52:15 +0530

    12thcommit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2106055765"
+    serviceWorkerVersion: "3700373153"
   }
 });
